a51c2abd33e4578e9545724bb40092e64255cd1c
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-06-04 00:40:36 +0800

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+	def index
+		@post = Post.new
+	end	
+end
---
+++ db/migrate/20170516014516_create_post.rb
@@ -0,0 +1,9 @@
+class CreatePost < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+    	t.string :title, null: false
+    	t.text :description, null: false
+    	t.timestamps null: false
+    end
+  end
+end
