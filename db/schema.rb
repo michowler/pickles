6fc7c6302b922a8644fd6bdd37850ae3dc13c61f
Michelle Ler <michelleler@Michelles-MacBook-Pro-2.local>
2018-05-29 12:04:45 +0800

    clean up

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,10 @@ ActiveRecord::Schema.define(version: 20170604153114) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",          null: false
-    t.text     "description",    null: false
-    t.string   "enter_password"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.string   "title",       null: false
+    t.text     "description", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "welcomes", force: :cascade do |t|
